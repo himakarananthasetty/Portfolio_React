989091207ef261366f3edefc3036287f47b0053b
Ananthasetty Himakar <himakarananthasetty@gmail.com>
2023-06-30 12:59:14 -0400

    First Comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills"
+import Work from "./components/Work";
+
+/* To do
+
+1) Links to social media
+2) Link to view work
+3) Projects and their or source code
+4) Deploy on Netlify*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1 className="text-2xl font-bold">Hello</h1> */}
+      <Navbar/>
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contacts />
     </div>
   );
 }
